48e16f7eee3fb29d445239ae6223e66c73a65b94
cheep <cheepaxxa2003@gmail.com>
2026-04-14 21:14:47 +0300

    Function for copping files

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    args = command.split()
+
+    if (len(args) != 3
+            or args[0] != "cp"
+            or args[1] == args[2]
+            or not os.path.exists(args[1])):
+        return
+
+    with open(args[1], "r") as file_in, open(args[2], "w") as file_out:
+        file_out.write(file_in.read())
